f9e15b01270ff75bf49795b6e8fb0716141a098b
Bhavik Makwana <bhavik.makwana@multidots.com>
2018-09-13 13:19:36 +0530

    app name change code formatting

--- lib/MovieItem.dart
+++ lib/MovieItem.dart
@@ -17,59 +17,77 @@ class MovieItem extends StatelessWidget {
       margin: EdgeInsets.all(16.0),
       child: InkWell(
         radius: 8.0,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Hero(
-              tag: movie.posterPath,
-              child: Container(
-                height: 200.0,
-                decoration: BoxDecoration(
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 8.0)],
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: DecorationImage(
-                      image: NetworkImage(
-                        "https://image.tmdb.org/t/p/w500/${movie.posterPath}",
-                      ),
-                      fit: BoxFit.cover),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    movie.title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  Text(
-                    movie.overview,
-                    softWrap: true,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Theme.of(context).textTheme.body2,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: getCardView(context),
         onTap: () {
+          //This widget is used to navigate from one screen to another
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (context) => MovieDetail(
+              builder: (context) =>
+                  MovieDetail(
                     movie: movie,
                   ),
             ),
           );
         },
       ),
+    );
+  }
+
+  /*
+  Returns the column containing card layout for the item list
+   */
+  getCardView(BuildContext context) {
+    return Column(
+      // Column start
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Hero(
+          // To do the animation
+          tag: movie.posterPath, // must be unique for the proper animation
+          child: Container(
+            //container for giving border to the image
+            height: 200.0,
+            decoration: BoxDecoration(
+              boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 8.0)],
+              borderRadius: BorderRadius.circular(8.0),
+              image: DecorationImage(
+                  image: NetworkImage(
+                    "https://image.tmdb.org/t/p/w500/${movie.posterPath}",
+                  ),
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                movie.title,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .title,
+              ),
+              Text(
+                movie.overview,
+                softWrap: true,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .body2,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Movie',
+      title: 'Flutter Movie', // title
       debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         primarySwatch: Colors.blueGrey,
